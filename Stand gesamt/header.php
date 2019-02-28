bd7e2885c3a65d289174732ac1892204eeb74afb
Linus Dehner <ldehner@student.tgm.ac.at>
2019-02-28 10:56:39 +0100

    delete all

--- Stand gesamt/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-    <nav>
-        <div class="nav-wrapper teal accent-4">
-            <a href="index.php?=home" class="brand-logo" style="margin-left: 15px">Time Planer</a>
-        </div>
-    </nav>
-</header>
