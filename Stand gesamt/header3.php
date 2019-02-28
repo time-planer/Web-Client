bc5cef0b7e6cbba23b70d7542ab165a9b4041bc7
Linus Dehner <ldehner@student.tgm.ac.at>
2019-02-28 11:04:49 +0100

    1. Hochladen, Stand 28.02.
    Bereitstellen der Daten, die bis 28.02. erstellt und bearbeitet wurden.

---
+++ Stand gesamt/header3.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <div class="nav-wrapper teal accent-4">
+            <a href="index.php?=home" class="brand-logo" style="margin-left: 15px">Time Planer</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="index.php?=groups">Übersicht</a></li>
+                <li><a href="index.php?=gedit">Bearbeiten</a></li>
+                <li><a href="index.php?=gnew">Erstellen</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ Stand gesamt/view.js
@@ -0,0 +1,7 @@
+function styleTask(task) {
+    var exceedDuration = task.children(".deadlineexceed").attr("duration");
+    var planExceedDuration = task.children(".planexceed").attr("duration");
+    var offset = task.attr("dayOffset");
+
+    //style mit diesen Werten
+}
