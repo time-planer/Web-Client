8ef443a4d1842b41bf3e864ff446c53fb4f8dd69
Nils Brugger <34156963+nbrugger-tgm@users.noreply.github.com>
2019-03-18 03:14:52 +0100

    Rework of onepage

--- js/onepage.js
+++ js/onepage.js
@@ -1,43 +1,50 @@
 const onepage = {};
-var dev = false;
+const dev = false;
 
 /**
  * main Init
  */
 (function(exports) {
-    var components = [];
-    var views = [];
-    if(dev){
+    const components = [];
+    if(dev)
         exports.components = components;
-        exports.views = views;
-    }
     /**
      * @param view {onepage.View}
      */
-    var defInit = function(view) {
+    const defInit = function(view) {
         view.get().find("[subst]").each(function() {
-            var val = $(this).attr("subst");
+            const val = $(this).attr("subst");
             $(this).text(view[val]);
         });
     };
+
+    exports.compPath = "components";
     /**
      *@param name {string}
-     * @return {exports.Component}
+     * @return {onepage.Component}
      */
     exports.getComp = function(name) {
-        for (var i = 0; i < components.length; i++) {
+        for (let i = 0; i < components.length; i++) {
             if (components[i].name === name) return components[i];
         }
     };
     /**
-     * @param root {jquery.fn.init}
+     * @param root {string | jquery.fn.init}
      */
     exports.loadComps = function(root = null) {
         if(root === undefined || root === null )
             root = $("body");
+        if(typeof  root === 'string') {
+            root = ("<div></div>").load(onepage.compPath+"/"+root+".html");
+        } else if(Array.isArray(name)){
+            for (let i = 0; i < name.length; i++) {
+                onepage.loadComp(name[i]);
+            }
+            return;
+        }
         root.find(".component").hide().each(function() {
-            var elem = $(this);
-            var comp = new exports.Component();
+            const elem = $(this);
+            const comp = new exports.Component();
             comp.name = elem.attr("name");
             comp.base = elem;
             comp.base.removeClass("component");
@@ -46,19 +53,18 @@ var dev = false;
         });
     };
     exports.substViews = function(element) {
-        var els;
+        let els;
         if(element !== undefined && element != null)
             els = element.find(".view");
         else
             els = $(".view");
         els.show().each(function() {
-            var elem = $(this);
-            var c, v;
-            for (var i = 0; i < components.length; i++) {
+            let elem = $(this);
+            let c, v;
+            for (let i = 0; i < components.length; i++) {
                 c = components[i];
                 if (c.name === elem.attr("name")) {
                     v = c.create();
-                    views.push(v);
                     $(elem[0].attributes).each(function() {
                         v[this.name] = this.value;
                     });
@@ -70,7 +76,6 @@ var dev = false;
             c = new exports.Component(elem.attr("name"));
             c.load();
             v = c.create();
-            views.push(v);
             $(elem[0].attributes).each(function() {
                 v[this.name] = this.value;
             });
@@ -83,131 +88,135 @@ var dev = false;
         this.substViews();
     };
     exports.loadComp = function(){
-        var tmp = this.compPath;
+        const tmp = this.compPath;
         if(arguments.length === 0)
             this.loadComps();
         else if(arguments.length === 1){
-            var c = new exports.Component(name);
+            const c = new this.Component(arguments[0]);
             c.load();
             components.push(c);
         }
         else{
-            for (var i = 0;i<arguments.length;i++){
+            for (let i = 0;i<arguments.length-1;i++){
+                this.compPath += "/";
                 this.compPath += arguments[i];
-                if(i+1 !== arguments.length)
-                    this.compPath += "/";
             }
-            var c = new exports.Component(name);
+            const c = new this.Component(arguments[arguments.length-1]);
             c.load();
             components.push(c);
         }
         this.compPath = tmp;
     };
-    exports.compPath = undefined;
-})(onepage);
 
-/**
- * Component
- */
-(function(exports) {
+
     /**
      * @param name {String}
      * @constructor
      */
-    exports.Component = function (name) {
-        if (name != null) {
-            this.name = name;
-            this.load();
-        }
-    };
-    /**
-     * @type {jquery.fn.init}
-     */
-    exports.Component.prototype.base = null;
-    exports.Component.prototype.load = function() {
-        this.base = $($.ajax({
-            type: "GET",
-            url: onepage.compPath+"/" + this.name + ".html",
-            async: false
-        }).responseText);
-    };
-    /**
-     * @return {onepage.View}
-     */
-    exports.Component.prototype.create = function() {
-       var v = new exports.View(this);
-       v.init();
-       return v;
-    };
-    /**
-     * @param v {onepage.View}
-     */
-    exports.Component.prototype.init = function(v) {};
-})(onepage);
+    exports.Component = class Component {
+        load(){
+            this.base = $($.ajax({
+                type: "GET",
+                url: onepage.compPath+"/" + this.name + ".html",
+                async: false
+            }).responseText);
+        };
+        /**
+         * @return {onepage.View}
+         */
+       create() {
+            var v = new exports.View(this);
+            v.init();
+            return v;
+        };
+        /**
+         * @param v {onepage.View}
+         */
+        init (v) {};
 
-/**
- * VIEW
- */
-(function(exports) {
-
-    /**
-     * @param comp {onepage.Component}
-     * @constructor
-     */
-    exports.View = function (comp) {
-        this.comp = comp;
-        if (this.comp != null) { this.element = comp.base.clone().show();}
-        var _this = this;
-        this.values = new Proxy({
-            ___THIS___ : _this
-        },exports.View.prototype.handler);
-    };
-    /**
-     * @return {jquery.fn.init}
-     */
-    exports.View.prototype.get = function() {
-        if (this.element == null) init();
-        return this.element;
-    };
-    /**
-     *
-     * @type {jquery.fn.init}
-     */
-    exports.View.prototype.element = null;
-    exports.View.prototype.refresh = function() {
-        this.element.html(this.comp.base.clone().html());
-        exports.substViews(this.element);
-        this.comp.init(this);
-    };
-    /**
-     * @return {jquery.fn.init}
-     */
-    exports.View.prototype.$ = exports.View.prototype.get;
-    exports.View.prototype.init = function() {
-        exports.substViews(this.element);
-        this.comp.init(this);
-    };
-    exports.View.prototype.handler = {
-        set: function(target, key, value) {
-            if(key === "___THIS___"){
-                return;
+        /**
+         * @param name
+         */
+        constructor (name = null){
+            /**
+             * @type {jquery.fn.init}
+             */
+            this.base = undefined;
+            /**
+             * @type {string}
+             */
+            this.name = undefined;
+            if (name != null) {
+                this.name = name;
+                this.load();
             }
-            console.log(`Setting value ${key} as ${value}`);
-            target[key] = value;
-            target.___THIS___.refresh();
         }
+
     };
     /**
-     * @param key {string}
-     * @param value {*}
-     * @return {*}
+     * @type {onepage.View}
      */
-    exports.View.prototype.val = function (key,value = null) {
-        if(value !== undefined && value !== null){
-            this.values[key] = value;
-            return value;
-        }else{
-            return this.values[key];
+    exports.View = class View {
+        /**
+         * @return {jquery.fn.init}
+         */
+        $(){
+            return this.get();
+        };
+        /**
+         * @return {jquery.fn.init}
+         */
+        get() {
+            if (this.element == null) init();
+            return this.element;
         }
-    }
+        refresh() {
+            this.element.html(this.comp.base.clone().html());
+            this.init();
+        }
+        init() {
+            exports.substViews(this.element);
+            this.comp.init(this);
+        }
+
+        /**
+         * @param key {string}
+         * @param value {*}
+         * @return {*}
+         */
+        val(key,value = null) {
+            if(value !== undefined && value !== null){
+                this.values[key] = value;
+            }else{
+                return this.values[key];
+            }
+        };
+        /**
+         * @param comp {onepage.Component}
+         * @constructor
+         */
+        constructor (comp){
+            this.comp = comp;
+            /**
+             * @type {jquery.fn.init}
+             */
+            this.element = undefined;
+            if (this.comp != null) { this.element = comp.base.clone().show();}
+            const _this = this;
+            this.values = new Proxy({
+                ___THIS___ : _this
+            },{
+                set: function(target, key, value) {
+                    if(key === "___THIS___"){
+                        return target.___THIS___;
+                    }
+                    console.log(`Setting value ${key} as ${value}`);
+                    target[key] = value;
+                    target.___THIS___.refresh();
+                    return value;
+                }
+            });
+        }
+
+    };
 })(onepage);
