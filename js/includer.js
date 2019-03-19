35d06f617cdbaa6c0d0e3a93ed597dd160f75ebc
Ryan <43230399+rfoster93@users.noreply.github.com>
2019-03-19 13:02:34 +0100

    corrected .md file
    yaaassss

--- js/includer.js
+++ js/includer.js
@@ -79,7 +79,6 @@ function showHome() {
                 var newname =  (JSON.parse(get_cookie("view"))[v.group.name]);
                 chkbox.prop('checked', newname);
             }
-
         };
         memgroup.getGroups(get_cookie("name"),function (a,data,c) {
             //TODO: Error checking
@@ -97,10 +96,11 @@ function showHome() {
                 settings[$(this).find(".grp-name").text()] = inp.get(0).checked;
             });
             set_cookie("view",JSON.stringify(settings));
+            loadView();
         });
     });
     includeBody("home",function () {
-        $("#out").click(logout);
+        $(".out").click(logout);
         $("#del").click(deleteTask);
         M.Modal.getInstance($('#modal1')).options.onCloseStart = editTask;
 		tasking.getAllTasks(get_cookie("name"),receiveAllTasks);
