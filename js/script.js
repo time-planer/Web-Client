523b5fe37d20a756e4dbd7a5b34131e446210a14
Nils Brugger <34156963+nbrugger-tgm@users.noreply.github.com>
2019-03-25 18:54:58 +0100

    URL Change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var timeplaner = require("time_planer");
 timeplaner.ApiClient.instance.basePath = "https://eds.logfro.de/time-planer/";
+//timeplaner.ApiClient.instance.basePath = "http://10.0.100.30:8080/eds/time-planer/";
 var auth = new timeplaner.AuthenticationApi();
 var tasking = new timeplaner.TaskingApi();
 var mygroups = new timeplaner.MyGroupsApi();
